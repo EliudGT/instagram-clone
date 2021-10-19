1d09d04b8a7403ecb5b82c6e736afdd9864eec92
Eliud <oeliud8@gmail.com>
2021-10-18 22:29:27 -0500

    second step of clonning instagram

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 
+import React, {useState} from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState ([
+    {
+      username: "Oscar",
+      caption: "Wowit work",
+      imageUrl:"https://ep1.pinkbike.org/p2pb21499691/p2pb21499691.jpg"
+    },
+    {
+      username: "Oscar",
+      caption: "Wowit work",
+      imageUrl:"https://ep1.pinkbike.org/p2pb21499691/p2pb21499691.jpg"
+    }
+  ]);
+
   return (
     <div className="App">
       <div className="app__header">
         <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="app__headerImage" />
       </div>
       <h1>Hola</h1>
-      <Post></Post>
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+      <Post username="Oscar" caption="Wowit work" imageUrl="https://ep1.pinkbike.org/p2pb21499691/p2pb21499691.jpg"/>
+      <Post username="Adriana" caption="Adriana caption" imageUrl="https://ep1.pinkbike.org/p6pb21489098/p6pb21489098.jpg"/>
+      <Post username="Eliud" caption="Eliud caption" imageUrl="https://ep1.pinkbike.org/p6pb21489218/p6pb21489218.jpg"/>
     </div>
     
   );
--- src/Post.js
+++ src/Post.js
@@ -2,27 +2,25 @@ import React from "react";
 import "./Post.css";
 import { Avatar } from "@mui/material";
 
-const Post = () => {
+const Post = ({username, caption, imageUrl}) => {
   return (
     <>
       <div className="post">
-        <div className="post__header"></div>
+        <div className="post__header">
         <Avatar
           alt="Remy Sharp"
+          className="post__avatar"
           src="/static/images/avatar/1.jpg"
           sx={{ width: 24, height: 24 }}
         />
+        <h3>{username}</h3>
+        </div>
+        
         {/*Header -> avatar + username*/}
-        <img
-          src="https://ep1.pinkbike.org/p2pb21499691/p2pb21499691.jpg"
-          alt=""
-          className="post__image"
-        />
+        <img src={imageUrl} className="post__image" alt="image" />
+        
         {/*image*/}
-        <h4 className="post__text">
-          {" "}
-          <strong>Username:</strong> caption
-        </h4>
+        <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
         {/*username + caption*/}
       </div>
     </>
